a89915f1028c92e3e3b79c631d0582addd305857
Parham Alvani <parham.alvani@gmail.com>
2024-02-14 21:35:04 +0000

    feat: add support for more icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,28 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      editLink: {
-        baseUrl: 'https://github.com/1995parham-learning/1995parham-learning.github.io/edit/main/',
-      },
-      logo: {
-        src: './src/assets/logo.png',
-      },
-      favicon: '/src/assets/logo.png',
-      title: "Parham's Notes",
-      social: {
-        github:
-          "https://github.com/1995parham-learning/1995parham-learning.github.io",
-      },
-      sidebar: [
-        {
-          label: "Programming Languages",
-          items: [{ label: "Golang", link: "/lang/go" }],
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    editLink: {
+      baseUrl: 'https://github.com/1995parham-learning/1995parham-learning.github.io/edit/main/'
+    },
+    logo: {
+      src: './src/assets/logo.png'
+    },
+    favicon: '/src/assets/logo.png',
+    title: "Parham's Notes",
+    social: {
+      github: "https://github.com/1995parham-learning/1995parham-learning.github.io"
+    },
+    sidebar: [{
+      label: "Programming Languages",
+      items: [{
+        label: "Golang",
+        link: "/lang/go"
+      }]
+    }]
+  }), icon()]
 });
