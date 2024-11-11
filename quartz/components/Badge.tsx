90ed464004cec38284bcc5f80e1a19336fa09a04
Parham Alvani <parham.alvani@gmail.com>
2024-11-11 18:40:34 +0000

    feat: update learning notes 2024-11-11 18:40:34

--- quartz/components/Badge.tsx
+++ quartz/components/Badge.tsx
@@ -1,10 +1,16 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Badge: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
-  return fileData.frontmatter?.repo ?
-    <a href={"https://github.com/"+fileData.frontmatter.repo}>
+
+  if (fileData.frontmatter?.repo && typeof(fileData.frontmatter.repo) === string) {
+    org = fileData.frontmatter.repo.split("/")
+
+    return <a href={"https://github.com/"+fileData.frontmatter.repo}>
       <img alt="Static Badge" src="https://img.shields.io/badge/Github-1995parham_learning-orange?style=for-the-badge&logo=github" />
-    </a> : null
+    </a>
+  }
+
+  return null
 }
 
 export default (() => Badge) satisfies QuartzComponentConstructor
