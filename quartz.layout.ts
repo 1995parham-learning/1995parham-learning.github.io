5d90487be3aeedf17b91e53f720f4250e4a1ffc1
Parham Alvani <parham.alvani@gmail.com>
2024-11-11 18:34:42 +0000

    feat: update learning notes 2024-11-11 18:34:42

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.Badge],
+  header: [Component.Badge()],
   afterBody: [],
   footer: Component.Footer({
     links: {
