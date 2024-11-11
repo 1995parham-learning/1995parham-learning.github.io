a434aabafb77266af5bdf89a1cf814e97c37b2f2
Parham Alvani <parham.alvani@gmail.com>
2024-11-11 18:46:36 +0000

    feat: update learning notes 2024-11-11 18:46:36

--- quartz/components/Badge.tsx
+++ quartz/components/Badge.tsx
@@ -1,13 +1,17 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Badge: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
-
-  if (fileData.frontmatter?.repo && typeof(fileData.frontmatter.repo) === "string") {
+  if (fileData.frontmatter?.repo && typeof fileData.frontmatter.repo === "string") {
     const org = fileData.frontmatter.repo.split("/")[0].replaceAll("-", "_")
 
-    return <a target="_blank" href={"https://github.com/"+fileData.frontmatter.repo}>
-      <img alt="Static Badge" src={`https://img.shields.io/badge/Github-${org}-orange?style=for-the-badge&logo=github`} />
-    </a>
+    return (
+      <a target="_blank" href={"https://github.com/" + fileData.frontmatter.repo}>
+        <img
+          alt="Static Badge"
+          src={`https://img.shields.io/badge/Github-${org}-orange?style=for-the-badge&logo=github`}
+        />
+      </a>
+    )
   }
 
   return null
